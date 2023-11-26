c4ab6b79e57eef76a75733dc172e350e1c80d58b
paulinechaga <paulinenyaboe0@gmail.com>
2023-11-26 13:41:22 +0300

    style

--- authentication/views.py
+++ authentication/views.py
@@ -51,54 +51,10 @@ def signup(request):
         myuser.save()
         messages.success(request, "Your Account has been created succesfully!!")
         
-        # Welcome Email
-        # subject = "Welcome to Django Login!!"
-        # message = "Hello " + myuser.first_name + "!! \n" + "Welcome to django \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nAnubhav Madhav"        
-        # from_email = settings.EMAIL_HOST_USER
-        # to_list = [myuser.email]
-        # send_mail(subject, message, from_email, to_list, fail_silently=True)
-        
-        # # Email Address Confirmation Email
-        # current_site = get_current_site(request)
-        # email_subject = "Confirm your Email @ GFG - Django Login!!"
-        # message2 = render_to_string('email_confirmation.html',{
-            
-        #     'name': myuser.first_name,
-        #     'domain': current_site.domain,
-        #     'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-        #     'token': generate_token.make_token(myuser)
-        # })
-        # email = EmailMessage(
-        # email_subject,
-        # message2,
-        # settings.EMAIL_HOST_USER,
-        # [myuser.email],
-        # )
-        # email.fail_silently = True
-        # email.send()
-        
         return redirect('signin')
         
         
     return render(request, "authentication/signup.html")
-
-
-# def activate(request,uidb64,token):
-#     try:
-#         uid = force_text(urlsafe_base64_decode(uidb64))
-#         myuser = User.objects.get(pk=uid)
-#     except (TypeError,ValueError,OverflowError,User.DoesNotExist):
-#         myuser = None
-
-#     if myuser is not None and generate_token.check_token(myuser,token):
-#         myuser.is_active = True
-#         # user.profile.signup_confirmation = True
-#         myuser.save()
-#         login(request,myuser)
-#         messages.success(request, "Your Account has been activated!!")
-#         return redirect('signin')
-#     else:
-#         return render(request,'activation_failed.html')
 
 
 def signin(request):
